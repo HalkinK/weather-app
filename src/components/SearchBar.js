1b8deeeaac4817f07ca7b550b0aa1dbdcc2d16cf
Konstantin Halkin <constantingal@gmail.com>
2021-08-31 14:45:04 +0300

    add hooks to SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,36 +1,32 @@
-import React from "react";
+import { useState } from "react";
 
-export default class SearchBar extends React.Component {
-  state = {
-    cityName: "",
+export default function SearchBar({ onSubmit }) {
+  const [cityName, setCityName] = useState("");
+
+  const handleNameChange = (event) => {
+    setCityName(event.currentTarget.value.toLowerCase());
   };
 
-  handleNameChange = (event) => {
-    this.setState({ cityName: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    if (this.state.cityName.trim() === "") {
+    if (cityName.trim() === "") {
       alert("Please enter your city");
       return;
     }
 
-    this.props.onSubmit(this.state.cityName);
-    this.setState({ cityName: "" });
+    onSubmit(cityName);
+    setCityName("");
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="cityName"
-          value={this.state.cityName}
-          onChange={this.handleNameChange}
-        />
-        <button type="submit">Show weather</button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="cityName"
+        value={cityName}
+        onChange={handleNameChange}
+      />
+      <button type="submit">Show weather</button>
+    </form>
+  );
 }
